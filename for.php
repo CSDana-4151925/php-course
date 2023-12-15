225a4b2eef32b98dfec20ac1112bb414b20cd1cd
CSDana-4151925 <tu4151925@taibahu.edu.sa>
2023-12-15 05:48:17 +0300

    first commit

---
+++ for.php
@@ -0,0 +1,11 @@
+<?php
+$family =['dana','Anas','abid'];
+
+foreach($family as $member){
+    echo $member .'<br>';
+}
+// for ($i=0; $i <count($family) ; $i++) { 
+//     echo $family[$i].'<br>';
+// }
+
+?>
---
+++ function.php
@@ -0,0 +1,23 @@
+<?php
+
+function add($x,$y){
+return $x+$y;
+}
+echo add(3,2);
+
+// function hello($x){
+//     $user =$x;
+// if ($user=="dana") {
+
+//   echo 'hello'.$user;
+// }else {
+//     echo 'Bye'.$user;
+// }
+// }
+// hello('dana');
+// echo '<br>';
+// hello('dana');
+// hello('dana');
+// hello('dana');
+
+?>
